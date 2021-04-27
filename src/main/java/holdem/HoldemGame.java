44b56a8777bb787430a181ac050226bac872aa90
forcheng <chuang@thoughtworks.com>
2021-04-27 22:11:13 +0800

    forcheng - add `canPlay`

--- src/main/java/holdem/HoldemGame.java
+++ src/main/java/holdem/HoldemGame.java
@@ -3,21 +3,22 @@ package holdem;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HoldemGame {
     private List<Player> pendingPlayers;
-    private List<BaseOperation> enableOperations;
     private Player progressPlayer;
     private List<Player> exitedPlayers;
     private double totalAmount;
+    private List<Operation> operationRecords;
 
     HoldemGame(List<Player> players) {
         List<Player> clonePlayers = new ArrayList<>(players);
         this.progressPlayer = clonePlayers.remove(0);
         this.pendingPlayers = clonePlayers;
-        this.enableOperations = Arrays.asList(BaseOperation.Pass, BaseOperation.Bet, BaseOperation.Fold);
         this.exitedPlayers = new ArrayList<>();
         this.totalAmount = 0;
+        this.operationRecords = new ArrayList<>();
     }
 
     public List<Player> getPendingPlayers() {
@@ -25,7 +26,25 @@ public class HoldemGame {
     }
 
     public List<BaseOperation> getEnableOperations() {
-        return enableOperations;
+
+        if (this.operationRecords.size() == 0) return Arrays.asList(BaseOperation.Pass, BaseOperation.Bet, BaseOperation.Fold);
+
+        List<BaseOperation> allOperations = Arrays.asList(BaseOperation.Pass, BaseOperation.Fold, BaseOperation.Bet, BaseOperation.Raise);
+        List<Operation> notFoldOperations = this.operationRecords.stream().filter(operation -> operation.getBaseOperation() != BaseOperation.Fold).collect(Collectors.toList());
+
+        int size = notFoldOperations.size();
+        if (size == 0) return allOperations;
+        Operation last = notFoldOperations.get(size - 1);
+
+        switch (last.getBaseOperation()) {
+            case Pass:
+                return allOperations;
+            case Bet:
+            case Raise:
+                return Arrays.asList(BaseOperation.Fold, BaseOperation.Bet, BaseOperation.Raise);
+            default:
+                return new ArrayList<>();
+        }
     }
 
     public Player getProgressPlayer() {
@@ -41,6 +60,7 @@ public class HoldemGame {
     }
 
     public void play(Operation operation) {
+        this.operationRecords.add(operation);
         BaseOperation baseOperation = operation.getBaseOperation();
         switch (baseOperation) {
             case Pass:
@@ -75,5 +95,9 @@ public class HoldemGame {
             winner.setAmount(winner.getAmount() + winAmount);
         }
         this.totalAmount = 0;
+    }
+
+    public boolean canPlay(Operation operation) {
+        return this.getEnableOperations().contains(operation.getBaseOperation());
     }
 }
