5d7e3f3a0c984c62a92045a373d5235eedb025ca
forcheng <chuang@thoughtworks.com>
2021-05-10 20:39:28 +0800

    forcheng - extract `getSortedCards` method to `AbstractRanking`

--- src/main/java/holdem/rankings/AbstractRanking.java
+++ src/main/java/holdem/rankings/AbstractRanking.java
@@ -3,6 +3,7 @@ package holdem.rankings;
 import holdem.enums.Suit;
 import holdem.models.Card;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,6 +11,10 @@ public abstract class AbstractRanking {
     public boolean isSameSuit(List<Card> cards) {
         List<Suit> distinctSuits = cards.stream().map(Card::getSuit).distinct().collect(Collectors.toList());
         return distinctSuits.size() == 1;
+    }
+
+    public List<Card> getSortedCards(List<Card> cards) {
+        return cards.stream().sorted(Comparator.comparingInt(Card::getNumber)).collect(Collectors.toList());
     }
 
     public abstract RankingResult parse(List<Card> cards);
--- src/main/java/holdem/rankings/StraightFlushRanking.java
+++ src/main/java/holdem/rankings/StraightFlushRanking.java
@@ -4,17 +4,15 @@ import holdem.enums.CardRanking;
 import holdem.models.Card;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
 
 public class StraightFlushRanking extends AbstractRanking {
     public RankingResult parse(List<Card> cards) {
         RankingResult result = null;
         if (this.isSameSuit(cards)) {
-            List<Integer> sortedNumbers = cards.stream().map(Card::getNumber).sorted().collect(Collectors.toList());
+            List<Card> sortedCards = this.getSortedCards(cards);
             boolean isStraightFlush = true;
-            for (int i = 0; i < sortedNumbers.size() - 1; i++) {
-                if (sortedNumbers.get(i) - sortedNumbers.get(i+1) != -1) {
+            for (int i = 0; i < sortedCards.size() - 1; i++) {
+                if (sortedCards.get(i).getNumber() - sortedCards.get(i+1).getNumber() != -1) {
                     isStraightFlush = false;
                     break;
                 }
