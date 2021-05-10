993857ce9a6e9812b62e47723f868051561c90ff
forcheng <chuang@thoughtworks.com>
2021-05-10 22:43:41 +0800

    forcheng - add case for `FullHouseRanking`

--- src/main/java/holdem/rankings/FullHouseRanking.java
+++ src/main/java/holdem/rankings/FullHouseRanking.java
@@ -8,9 +8,16 @@ import java.util.List;
 public class FullHouseRanking extends AbstractRanking {
     public RankingResult parse(List<Card> cards) {
         List<Card> sortedCards = this.getSortedCards(cards);
-        if (sortedCards.get(0).getNumber() == sortedCards.get(1).getNumber() &&
+
+        boolean isBefore3After2 = sortedCards.get(0).getNumber() == sortedCards.get(1).getNumber() &&
                 sortedCards.get(1).getNumber() == sortedCards.get(2).getNumber() &&
-                sortedCards.get(3).getNumber() == sortedCards.get(4).getNumber()) {
+                sortedCards.get(3).getNumber() == sortedCards.get(4).getNumber();
+
+        boolean isBefore2After3 = sortedCards.get(0).getNumber() == sortedCards.get(1).getNumber() &&
+                sortedCards.get(2).getNumber() == sortedCards.get(3).getNumber() &&
+                sortedCards.get(3).getNumber() == sortedCards.get(4).getNumber();
+
+        if (isBefore3After2 || isBefore2After3) {
             return new RankingResult(CardRanking.FULL_HOUSE);
         }
         return null;
--- src/test/java/holdem/rankings/FullHouseRankingTest.java
+++ src/test/java/holdem/rankings/FullHouseRankingTest.java
@@ -44,7 +44,7 @@ public class FullHouseRankingTest {
     }
 
     @Test
-    public void should_return_FULL_HOUSE_when_three_cards_are_same_cards_and_other_two_cards_also_are_same() {
+    public void should_return_FULL_HOUSE_when_three_cards_are_4_and_other_two_cards_are_5() {
         FullHouseRanking ranking = new FullHouseRanking();
         List<Card> cards = new ArrayList<Card>(5){
             {
@@ -53,6 +53,23 @@ public class FullHouseRankingTest {
                 add(new Card(5, Suit.Club));
                 add(new Card(4, Suit.Diamond));
                 add(new Card(5, Suit.Heart));
+            }
+        };
+        RankingResult result = ranking.parse(cards);
+        assertEquals(CardRanking.FULL_HOUSE, result.getCardRanking());
+    }
+
+
+    @Test
+    public void should_return_FULL_HOUSE_when_three_cards_are_4_and_other_two_cards_are_1() {
+        FullHouseRanking ranking = new FullHouseRanking();
+        List<Card> cards = new ArrayList<Card>(5){
+            {
+                add(new Card(4, Suit.Club));
+                add(new Card(4, Suit.Heart));
+                add(new Card(1, Suit.Club));
+                add(new Card(4, Suit.Diamond));
+                add(new Card(1, Suit.Heart));
             }
         };
         RankingResult result = ranking.parse(cards);
