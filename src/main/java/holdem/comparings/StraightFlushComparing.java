13a92d3c81e367a76a6de6828c3e89c6ba6d2eb8
forcheng <chuang@thoughtworks.com>
2021-05-11 08:30:47 +0800

    forcheng - refactor `StraightFlushComparing`

--- src/main/java/holdem/comparings/StraightFlushComparing.java
+++ src/main/java/holdem/comparings/StraightFlushComparing.java
@@ -8,15 +8,6 @@ public class StraightFlushComparing extends AbstractComparing {
     public int compare(List<Card> first, List<Card> second) {
         List<Card> sortedFirstCards = this.getSortedCards(first);
         List<Card> sortedSecondCards = this.getSortedCards(second);
-
-        if (sortedFirstCards.get(0).getNumber() < sortedSecondCards.get(0).getNumber()) {
-            return -1;
-        }
-
-        if (sortedFirstCards.get(0).getNumber() > sortedSecondCards.get(0).getNumber()) {
-            return 1;
-        }
-
-        return 0;
+        return Integer.compare(sortedFirstCards.get(0).getNumber(), sortedSecondCards.get(0).getNumber());
     }
 }
