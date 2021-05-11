6e3fe72200dd05e55cc135302262952350106263
forcheng <chuang@thoughtworks.com>
2021-05-11 19:56:51 +0800

    forcheng - add `FlushComparing`

---
+++ src/main/java/holdem/comparings/FlushComparing.java
@@ -0,0 +1,21 @@
+package holdem.comparings;
+
+import holdem.models.Card;
+
+import java.util.List;
+
+public class FlushComparing extends AbstractComparing {
+    public int compare(List<Card> first, List<Card> second) {
+        List<Card> sortedFirstCards = this.getSortedCards(first);
+        List<Card> sortedSecondCards = this.getSortedCards(second);
+
+        for(int i = 4; i >= 0; i--) {
+            int result = Integer.compare(sortedFirstCards.get(i).getNumber(), sortedSecondCards.get(i).getNumber());
+            if (result != 0) {
+                return result;
+            }
+        }
+
+        return 0;
+    }
+}
